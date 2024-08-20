aff2ebef5a6608660b56c4bcd1d35e9646b54599
Muhammad Ammar <ammarbinakram16@gmail.com>
2024-08-20 19:00:54 +0500

    Added comments.

--- blogs/views.py
+++ blogs/views.py
@@ -11,22 +11,24 @@ from django.contrib import messages
 
 # Create your views here
 
+# Views to create the sign up page of website
 class SignUpView(CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy("login")
     template_name = "registration/signup.html"
 
-
+# view where the logged in user first comes after log in, it shows all the posts of each user
 def home(request):
     blogs = Blog.objects.annotate(num_likes=Count("likes"))
     context = {'blogs': blogs}
     return render(request, 'blogs/home.html', context)
 
-
+# the landing page of the website
 def landing(request):
     return render(request, 'blogs/landing.html')
 
-
+# view that renders the form to create a post, it takes in user id as an argument and then checks if the form is filled or not, if filled it 
+# saves the post, if not it renders the create post page 
 def create_post(request, user_id):
     user = User.objects.get(pk=user_id)
     context_create = {'user': user}
@@ -44,6 +46,7 @@ def create_post(request, user_id):
         return render(request, 'blogs/create.html', context=context_create)
 
 
+# view that renders the blog that the logged in user wants to read, it takes in user id and blog id as an argument
 def read_blog(request, blog_id, user_id):
     blog = Blog.objects.get(pk=blog_id)
     user_liked = Likes.objects.filter(blog_id=blog_id, user_id=user_id).exists()
@@ -51,46 +54,53 @@ def read_blog(request, blog_id, user_id):
     return render(request, 'blogs/read.html', context)
 
 
+# view that renders the profile page of the logged in user, it takes in user id as an argument
 def view_profile(request, user_id):
     user = User.objects.get(id=user_id)
     blogs = Blog.objects.filter(user=user_id)
-    total_likes = Likes.objects.filter(user=user).count()
-    followers = Followers.objects.filter(user=user).count()
-    following = Followers.objects.filter(follower=user).count()
-    latest_blog = blogs[0]
+    total_likes = Likes.objects.filter(user=user).count() # likes that the user has received
+    followers = Followers.objects.filter(user=user).count() # followers of the user
+    following = Followers.objects.filter(follower=user).count() # users that the user is following
+    latest_blog = blogs[0] # the line and for loop is to find the latest blog of the user
     for blog in blogs:
         if blog.created_at > latest_blog.created_at:
             latest_blog = blog
-    context = {'user': user, 'blogs': blogs, 'latest_blog': latest_blog, 'total_likes': total_likes, 'followers': followers, 'following': following}
+    # context variables that are passed to the template
+    context = {'user': user, 'blogs': blogs, 'latest_blog': latest_blog, 'total_likes': total_likes, 'followers': followers, 'following': following} 
     return render(request, 'blogs/profile.html', context)
 
 
+# view for liking a post, it takes user id and blog id as argument
 def like_post(request, blog_id, user_id):
     blogId = user_id
     userId = blog_id
     blog = Blog.objects.get(pk=blogId)
     user = User.objects.get(pk=userId)
-    like = Likes(blog=blog, user=user)
-    like.save()
-    messages.success(request, f"You liked {blog.user.username}'s {blog.title} ")
+    like = Likes(blog=blog, user=user) # like object is created 
+    like.save() # like object is saved in db
+    messages.success(request, f"You liked {blog.user.username}'s {blog.title} ") # message that will be shown after the post is liked
     return redirect('home')
 
 
+# view for unliking a post, it takes user id and blog id as an argument
 def unlike_post(request, user_id, blog_id):
     blog = Blog.objects.get(pk=blog_id)
     user = User.objects.get(pk=user_id)
-    like = Likes.objects.get(blog=blog, user=user)
-    like.delete()
+    like = Likes.objects.get(blog=blog, user=user) # like object is fetched form db
+    like.delete() # like object deleted
     messages.warning(request, f"You unliked {blog.user.username}'s post {blog.title} ")
     return redirect('home')
 
 
+# view to show the logged in user all his posts, it takes user id as an argument
 def view_your_posts(request, user_id):
     blogs = Blog.objects.filter(user=user_id)
     context = {'blogs': blogs}
     return render(request, 'blogs/user_posts.html', context)
 
 
+# view to edit a post, it takes blog id as an argument, checks if the form is filled, if yes then, saves the edited post, if not then 
+# renders the edit page
 def edit_post(request, blog_id):
     blog = Blog.objects.get(pk=blog_id)
     context = {'blog': blog}
@@ -105,6 +115,7 @@ def edit_post(request, blog_id):
         return render(request, 'blogs/edit.html', context)
 
 
+# view to delete a post, it takes blog id as an argument, checks if the method is post, if yes then deletes the post, else render the delete page
 def delete_post(request, blog_id):
     blog = Blog.objects.get(pk=blog_id)
     context = {'blog': blog}
@@ -116,23 +127,27 @@ def delete_post(request, blog_id):
         return render(request, 'blogs/delete.html', context)
 
 
+# view to show the profile of other users, it takes blog id, user id as an argument
 def others_profile(request, blog_id, user_id):
-    blog = Blog.objects.get(pk=blog_id)
-    logged_in_user = User.objects.get(pk=user_id)
+    blog = Blog.objects.get(pk=blog_id) 
+    logged_in_user = User.objects.get(pk=user_id) # user that is logged in
     other_user = blog.user # user whose profile is being viewed
-    blogs_user = Blog.objects.filter(user=other_user)
-    total_likes = Likes.objects.filter(user=other_user).count()
-    user_followers = Followers.objects.filter(user=other_user).count()
-    user_following = Followers.objects.filter(follower=other_user).count()
-    follows = Followers.objects.filter(follower=logged_in_user, user=other_user).exists()
-    latest_blog = blogs_user[0]
+    blogs_user = Blog.objects.filter(user=other_user) # all posts of the user whose profile is being viewed
+    total_likes = Likes.objects.filter(user=other_user).count() # all likes of the user whose profile is being viewed
+    user_followers = Followers.objects.filter(user=other_user).count() # all followers of the user whose profile is being viewed
+    user_following = Followers.objects.filter(follower=other_user).count() # all following of the user whose profile is being viewed
+    follows = Followers.objects.filter(follower=logged_in_user, user=other_user).exists() # checks if the logged in user follows the other user or not
+    latest_blog = blogs_user[0] # this line and for loop is to find the latest blog of the user whose profile is being viewed
     for blog in blogs_user:
         if blog.created_at > latest_blog.created_at:
             latest_blog = blog
+    # context variables being passed to the template
     context = {'other_user': other_user, 'blogs': blogs_user, 'latest_blog': latest_blog, 'total_likes': total_likes, 'followers': user_followers, 'following': user_following, 'follows': follows}
     return render(request, 'blogs/others_profile.html', context)
 
 
+# view to follow any user, it takes follower id, user id as an argument, user id refers to the user that is being followed, follower id is the 
+# id of the user who is following
 def follow_user(request, follower_id, user_id):
     followed_user = User.objects.get(pk=user_id)
     follower = User.objects.get(pk=follower_id)
@@ -141,7 +156,8 @@ def follow_user(request, follower_id, user_id):
     messages.success(request, f'You followed {followed_user.username}')
     return redirect('home')
 
-
+# view to unfollow any user, it takes follower id, user id as an argument, user id refers to the user that is being followed, follower id is the 
+# id of the user who is following
 def unfollow_user(request, follower_id, user_id):
     followed_user = User.objects.get(pk=user_id)
     follower = User.objects.get(pk=follower_id)
